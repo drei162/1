bbd600dd133447078e4fc93468656b07c22aa9a4
Aldrei Simbre <aldrei.john7134@gmail.com>
2025-10-24 21:39:03 +0800

    Update OneProcessThreads.cpp

--- OneProcessThreads.cpp
+++ OneProcessThreads.cpp
@@ -26,11 +26,11 @@ int main()
 {
 
 	std::thread add(NumberOne);
-	//std::this_thread::sleep_for(std::chrono::seconds(1));
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 	std::thread addition(NumberTwo);
-	//std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::this_thread::sleep_for(std::chrono::seconds(2));
 	std::thread multiply(NumberThree);
-	//std::this_thread::sleep_for(std::chrono::seconds(3));
+	std::this_thread::sleep_for(std::chrono::seconds(3));
 	multiply.join();
 	add.join();
 	addition.join();
